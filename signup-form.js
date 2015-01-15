e3d94a8b181a5ce64ef70ccc4c2ac5a8a6ae4aad
Rob Christian <rm.rf.etc@gmail.com>
2015-01-15 04:09:51 -0800

    Better use of comparison func.

--- signup-form.js
+++ signup-form.js
@@ -36,73 +36,45 @@ http://inimino.org/~inimino/blog/javascript_semicolons
 
   concise.controller('home',function(o){
 
-    // function validEmail(prop, el1, el2){
-    //   var msg
-    //   return function() {
-    //     msg = /^\w+@\w+\.[\w.]+[^.]$/.test(this[prop]) ? '' : 'Input is not a valid email.'
-    //     el1.setCustomValidity( msg )
-    //     el2.setCustomValidity( msg )
-    //     console.log( el1.name, el2.name, 'VALIDITY:', el1.checkValidity(), el2.checkValidity() )
-    //   }
-    // }
-    // function validPassword(prop, el1, el2){
-    //   var msg
-    //   return function(){
-    //     msg = this[prop].length > 4 && this[prop].length < 20 ? '' : 'Password must be between 4 and 20 characters long.'
-    //     el1.setCustomValidity( msg )
-    //     el2.setCustomValidity( msg )
-    //     console.log( el1.name, el2.name, 'VALIDITY:', el1.checkValidity(), el2.checkValidity() )
-    //   }
-    // }
-
     o.view = document.querySelector('#view')
 
-    function compare(model, prop1, prop2){
-      return model[prop1] === model[prop2]
+    function getComparator(model, prop1, prop2){
+      return function(){ return model[prop1] === model[prop2] }
     }
 
     o.dom = {
     "row.width-12.column":{
-      "section.width-6.centered":
-      function(o){
+      "section.width-6.centered":function(o){
         var signup_form_el
         var signin_form_el
         var confirm_username_el
         var confirm_password_el
 
         o.dom = {
-        "form 1 validate":
-        function(o){
+        "form 1 validate":function(o){
           signup_form_el = this
 
           o.dom = {
           "label innerHTML='Email'":0,
           "input type='email' name='name' pattern='^\\w+@\\w+\\.[\\w.]+[^.]$' required":0,
           "label innerHTML='Confirm Email'":0,
-          "input type='email' name='_name' required":
-          function(o){
+          "input type='email' name='_name' required":function(o){
             confirm_username_el = this
           },
-
           "label innerHTML='Password'":0,
           "input type='password' name='pass' pattern='.{5,20}' required":0,
           "label innerHTML='Confirm Password'":0,
-          "input type='password' name='_pass' pattern='.{5,20}' required":
-          function(o){
+          "input type='password' name='_pass' pattern='.{5,20}' required":function(o){
             confirm_password_el = this
           },
-
-          "button.left innerHTML='Sign-In'":
-          function(o){
+          "button.left innerHTML='Sign-In'":function(o){
             o.onClick(function(ev){
               ev.preventDefault()
               signup_form_el.style.display = 'none'
               signin_form_el.style.display = 'block'
             })
           },
-
-          "button.right innerHTML='Submit'":
-          function(o){
+          "button.right innerHTML='Submit'":function(o){
             o.onClick(function(ev){
               ev.preventDefault()
               window.alert('Is valid?', o.el.checkValidity())
@@ -110,20 +82,22 @@ http://inimino.org/~inimino/blog/javascript_semicolons
           }}
 
 
+          var test_un = getComparator(o.model,'name','_name')
+
           o.model.onChange([ 'name', '_name' ], function(){
-            confirm_username_el.setCustomValidity( compare(o.model,'name','_name') ? '' : 'Entries do not match.' )
+            confirm_username_el.setCustomValidity( test_un() ? '' : 'Entries do not match.' )
             console.log( o.el.checkValidity() )
           })
 
+          var test_pw = getComparator(o.model,'pass','_pass')
+
           o.model.onChange([ 'pass', '_pass' ], function(){
-            confirm_password_el.setCustomValidity( compare(o.model,'pass','_pass') ? '' : 'Entries do not match.' )
+            confirm_password_el.setCustomValidity( test_pw() ? '' : 'Entries do not match.' )
             console.log( o.el.checkValidity() )
           })
 
         },
-
-        "form 2 validate style.display='none' validate":
-        function(o){
+        "form 2 validate style.display='none' validate":function(o){
           signin_form_el = this
 
           o.dom = {
@@ -131,16 +105,13 @@ http://inimino.org/~inimino/blog/javascript_semicolons
           "input type='email' name='name' required pattern='^\\w+@\\w+\\.[\\w.]+[^.]$'":0,
           "label innerHTML='Password'":0,
           "input type='password' name='pass' required":0,
-          "button.left innerHTML='Sign-Up'":
-          function(o){
+          "button.left innerHTML='Sign-Up'":function(o){
             o.onClick(function(ev){ ev.preventDefault()
               signin_form_el.style.display = 'none'
               signup_form_el.style.display = 'block'
             })
           },
-
-          "button.right innerHTML='Submit'":
-          function(o){
+          "button.right innerHTML='Submit'":function(o){
             o.onClick(function(ev){ ev.preventDefault()
               window.alert('Not implemented yet.')
             })
