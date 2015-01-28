f30ba369e3591c9b0348fedca9fbc8870333e72a
Rob Christian <rm.rf.etc@gmail.com>
2015-01-28 01:59:47 -0800

    Import link handling from Moz pushState example.

--- libs/runway-browser.js
+++ libs/runway-browser.js
@@ -1,6 +1,19 @@
 
-// var router = require('./runway.js')
+var router = require('./runway.js')
 
 window.onpopstate = function(event){
-  window.alert("location: " + document.location + ", state: " + JSON.stringify(event.state))
+  window.alert('location: ' + document.location + ', state: ' + JSON.stringify(event.state))
 }
+
+
+function processLink(){
+  if (/^\//.test(this.href) || this.href.indexOf(location.host) > -1) {
+    router.listener({ url:this.url }, null)
+    return false
+  }
+  return true
+}
+function init(){
+  for (var i = 0, length = document.links.length; i < length; document.links[i++].onclick = processLink);
+}
+window.addEventListener ? addEventListener('load', init, false) : window.attachEvent ? attachEvent('onload', init) : (onload = init)
