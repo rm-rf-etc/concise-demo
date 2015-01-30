b83aed401a78906d69ff24b86613c7f8323f0290
Rob Christian <rm.rf.etc@gmail.com>
2015-01-30 04:29:55 -0800

    Fix bug in popstate navigating.

--- libs/runway-browser.js
+++ libs/runway-browser.js
@@ -18,29 +18,38 @@ function processLink(){
   var href = this.href.replace(location.origin,'')
   // console.log('processLink', this.href, href)
   if (this.dataset.ajax !== 'none') {
-    goto(href)
+    goForward(href)
+    doRoute(href)
     return false
   }
   return true
 }
 
-function goto(href){
+function doRoute(href){
   var ctrl = runway.finder(href)
-  if (ctrl) {
-    ctrl()
-    updateUrl(href)
-  }
+  if (ctrl) ctrl()
 }
 
-function updateUrl(url){
-  if (history.pushState) history.pushState(null, '', url)
+function goForward(url){
+  var title = Math.random().toString().split('.')[1]
+  if (history.pushState) history.pushState({url:url, title:title}, null, url)
   else location.assign(url)
 }
 
+window.addEventListener('popstate',function(event){
+  doRoute(event.state.url)
+})
+
 window.onpopstate = function(event){
-  window.alert('location: ' + document.location + ', state: ' + JSON.stringify(event.state))
+  // doRoute(event.state.url)
+  // console.log( event )
+  // window.alert('location: ' + document.location + ', state: ' + JSON.stringify(event.state))
 }
 
-function init(){ goto(location.pathname) }
+function init(){
+  history.replaceState( {url:location.pathname}, null, location.pathname )
+  var ctrl = runway.finder(location.pathname)
+  if (ctrl) ctrl()
+}
 // setTimeout(init,100)
 window.addEventListener ? addEventListener('load', init, false) : window.attachEvent ? attachEvent('onload', init) : (onload = init)
