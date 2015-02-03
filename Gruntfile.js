48a00a057af7b70f5485482d32576e1f6f59ff22
Rob Christian <rm.rf.etc@gmail.com>
2015-02-02 18:14:26 -0800

    Make browsersync work with all routes and restart on gruntfile changes.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,40 +1,65 @@
-module.exports = function (grunt) {
-    'use strict';
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-browserify');
-    grunt.initConfig({
-        browserify: {
-            'dev': {
-                'src': './main.js',
-                'dest': 'public/scripts/bundle.js',
-                'options': {
-                    'debug': true,
-                    'watch': true,
-                    'verbose': true,
-                    'open': true
-                }
-            },
-            'release': {
-                'src': './main.js',
-                'dest': 'public/scripts/bundle.js',
-                'options': {
-                    'debug': false,
-                    'verbose': false
-                }
-            }
-        },
-        connect: {
-            'devServer': {
-                'options': {
-                    'base': 'app/',
-                    'keepalive': true
-                }
-            }
+
+var index = require('fs').readFileSync('./public/index.html')
+// var browserSync = require('browser-sync')
+// var browserSyncGrunt = require('grunt-browser-sync')
+
+module.exports = function(grunt){
+  'use strict'
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-browser-sync')
+  grunt.loadNpmTasks('grunt-browserify')
+  grunt.initConfig({
+    watch:{
+      source:{
+        files: ['main.js', 'libs/*', 'ui/*'],
+        tasks: ['browserify']
+      },
+      browsersync:{
+        files: ['Gruntfile.js'],
+        tasks: ['default']
+      }
+    },
+    browserify:{
+      dev:{
+        src: './main.js',
+        dest: 'public/scripts/bundle.js',
+        options:{
+          debug: true,
+          watch: false,
+          verbose: true,
+          open: true
         }
-    });
-    grunt.registerTask('default', [
-        'browserify:dev',
-        'connect'
-    ]);
-    grunt.registerTask('release', ['browserify:release']);
-};
+      },
+      release:{
+        src: './main.js',
+        dest: 'public/scripts/bundle.js',
+        options:{
+          debug: false,
+          verbose: false
+        }
+      }
+    },
+    browserSync:{
+      bsFiles:{
+        src: ['public/scripts/*', 'public/styles/*', 'public/index.html']
+      },
+      options:{
+        open: false,
+        watchTask: true,
+        server:{
+          baseDir: './public',
+          middleware: function(req, res, next){
+            if (/^\/styles|^\/scripts|^\/favicon.ico/g.test(req.url)) next()
+            else {
+              // console.log( 'index!', req.url )
+              res.writeHeader(200, { "Content-Type":"text/html" })
+              res.end( index )
+            }
+          }
+        }
+      }
+    }
+  })
+  grunt.registerTask('default', ['browserify:dev', 'browserSync', 'watch'])
+  grunt.registerTask('release', ['browserify:release'])
+}
